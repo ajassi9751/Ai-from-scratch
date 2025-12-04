3f41f98f103f5eb0b9c2fe3fa7e8b863c51b3562
ajassi9751 <ajassi9751@gmail.com>
2025-12-04 18:50:17 +0000

    Added intmatrix for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include "matrix.hpp"
 #include "number.hpp"
+#include "intmatrix.hpp"
+#include <vector>
+#include <utility>
 #include <stdio.h>
 #include <iostream>
 
 int main () {
-	matrix mat;
-	number num(3.14);
-	num.print();
+	std::vector<std::vector<int>> input;
+	std::vector<std::vector<int>> inputother;
+	std::vector<int> temp;
+	temp.push_back(3);
+	input.push_back(temp);
+	inputother.push_back(temp);
+	intmatrix mat(std::move(input));	
+	intmatrix matother(std::move(inputother));
+	mat = mat+ matother;
+	//matrix mat;
+	// number num(3.14);
+	// num.print();
 	return 0;
 }
